c171a58d432e8074a1edab8e78487e8b98016c68
Michał Ordon <michal@tehidea.com>
2016-04-23 22:44:50 +0100

    Rename file

--- client/App.jsx
+++ client/App.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    render() {
-        return (
-            <h1>Hello world</h1>
-        )
-    }
+  render() {
+    return (
+      <h1>Hello world</h1>
+    );
+  }
 }
 
 Meteor.startup(() => {
-    ReactDOM.render(<App />, document.getElementById('app'));
+  ReactDOM.render(<App />, document.getElementById('app'));
 });
