73107a3e1b67c4c335152cb7b1be959da39c259d
Michał Ordon <michal@tehidea.com>
2016-05-03 23:35:37 +0100

    Add a route with a parameter

---
+++ client/resolutions/ResolutionDetail.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import TrackerReact from 'meteor/ultimatejs:tracker-react';
+
+export default class ResolutionDetail extends TrackerReact(Component) {
+
+  constructor() {
+    super();
+
+    this.state = {
+      subscription: {
+        resolutions: Meteor.subscribe('userResolutions'), // This works thanks to TrackerReact
+      },
+    };
+  }
+
+  componentWillUnmount() {
+    this.state.subscription.resolutions.stop();
+  }
+
+  resolution() {
+    return Resolutions.findOne(this.props.id);
+  }
+
+  render () {
+    let res = this.resolution();
+
+    if (!res) {
+      return (<div>Loading...</div>);
+    }
+
+    return (
+      <div>
+        <h1>{res.text}</h1>
+      </div>
+    )
+  }
+}
